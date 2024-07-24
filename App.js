e28fa0ac793a89e7a1035d98f379229c4221caed
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-07-23 19:46:55 -0500

    created index file in the publick folder and updated json.

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // Import necessary React libraries and components
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from "./components/Header";
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
+import Header from "./src/components/Header";
+import About from './src/components/About';
+import Portfolio from './src/components/Portfolio';
+import Contact from './src/components/Contact';
+import Resume from './src/components/Resume';
+import Footer from './src/components/Footer';
 
 // Import Bootstrap and custom CSS styles
 import 'bootstrap/dist/css/bootstrap.min.css';
